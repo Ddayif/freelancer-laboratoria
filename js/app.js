79536e8b04df13e0c3e88c3f5d7e0099deffe5c6
Joselin Grez <joselingrez.m@hotmail.es>
2017-06-01 13:28:16 -0300

    Agregando nav

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 /*
  * Archivo principal de JS
  */
+
+ (function main(){
+ 	var trabajos = Array.from(document.getElementsByClassName("col-4"));
+ 	var nuevoDiv = document.getElementById("box-modal");
+ 	var mostrar, cerrar;
+ 	trabajos.forEach(function(el){
+ 		el.addEventListener("click", function(){
+ 			nuevoDiv.innerHTML = "";
+
+ 			mostrar = document.createElement("div");//crea un nuevo div dentro del ya existente
+ 			mostrar.classList.add("modal");
+ 			mostrar.innerHTML= el.innerHTML;// pega la clase encima del div vacio
+
+ 			
+ 		});
+ 	});
+
+ })()
